7c19ebe28adff014f0a7ab90fea9943896344c85
Joseph Bamber <rc0032091@student.riversidecollege.ac.uk>
2018-11-19 15:38:13 +0000

    Introduced bugs

--- Hello2/Form1.cs
+++ Hello2/Form1.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Hello2
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello);
         }
     }
 }
